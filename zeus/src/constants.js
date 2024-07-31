a3ea5678498a6a82138a5de01d97a92b4bec8fbe
Miks <michaelangelocasal@gmail.com>
2024-07-31 18:50:19 +0800

    first commit

---
+++ zeus/src/constants.js
@@ -0,0 +1,24 @@
+export const scaleFactor = 3.5;
+
+export const dialogueData = Object.freeze({
+  pc: `Hey, you found my PC! I work mostly in React, TypeScript, Vue with a little bit of PHP laravel these days.
+    Here is my <a href="https://github.com/mikskywalker" target="_blank">Github</a>!`,
+  certifications: `I hung my certificates on the wall because I'm proud of what I was able to accomplish! If you're interested, you can find some of em on <a href="https://coursera.org/share/cfaa448cb8b931dc711dde9bd48d94fc" target="_blank">Coursera</a> `,
+  couch: `That's my couch. I like to relax here, drink coffee, and watch youtube vlogs, anime, food shows, travel, music and many more. 
+    Speaking of yt, I also make violin covers, and post my compositions there! Check out my channel 
+    <a href="https://www.youtube.com/channel/UCPfm7j1Wm-S7hmUgk49nf8g" target="_blank">here</a>. 
+    Sub if you like the content! You'll find the bgm you are hearing there along with my other originals and covers!`,
+  tv: `You found my TV! Hahaha. Some of my all time favorite shows are boku-no-p... I mean... Frieren, Fairytail, Naruto, and Black Clover. 
+    There are also many good recent animes like Wyztoria and I Parry Eveything! I recommend them if you're looking for shows to watch!`,
+  bed: `Ah yes - my bed. A place of rest, tranquilty and.. playing nintendo switch.`,
+  resume: `You found my resume! 
+    <a href="https://drive.google.com/file/d/1UIrzNoTqN4yu7aJ_y7CSq7LKjjndl_4S/view?usp=sharing" target="_blank">Check it out?</a>
+    Contact me at michaelangelocasal@gmail.com if you have any interesting job opportunities!
+    p.s. the paper on the left contains info about my projects.`,
+  projects: `This portfolio is made with the Kaboom.js which is a library for making games with JavaScript. I've always wanted to create something like this when I first started learning how to code so this is great!
+    I'll surely update this, maybe even change the scene in the near future. I have another live portfolio you can find 
+    <a href="https://mikskywalker.github.io/portfolio/" target="_blank">here</a> if you're keen. It's built with react, typescript and material ui.`,
+  library: `There are a lot of self-help, lord of the rings, and financial books on my shelves. There are also japanese langugae books because I'm currenlty learning nihongo.`,
+  exit: `If you want to exit Miks's portfolio, just close the tab.`,
+  fridge: `Getting hungry? Maybe, it's a good time for a break. :P`,
+});
